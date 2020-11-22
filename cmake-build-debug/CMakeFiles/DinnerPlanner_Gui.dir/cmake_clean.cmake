02a9f887e5cef7284d434dcfac5ee4b98d790c79
rhys <email.curly@gmail.com>
2020-11-22 17:43:56 +0000

    Initial commit, still templating, but using some ugly plain C code for code i don't know in gtkmm.

---
+++ cmake-build-debug/CMakeFiles/DinnerPlanner_Gui.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DinnerPlanner_Gui.dir/DB.cpp.o"
+  "CMakeFiles/DinnerPlanner_Gui.dir/Recipe_View.cpp.o"
+  "CMakeFiles/DinnerPlanner_Gui.dir/main.cpp.o"
+  "CMakeFiles/DinnerPlanner_Gui.dir/sqlite/sqlite3.c.o"
+  "DinnerPlanner_Gui"
+  "DinnerPlanner_Gui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/DinnerPlanner_Gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
